787e1e2f53785c14264c57d17dd7da8dc359ca56
Shiven Sharma <shiven676@gmail.com>
2026-05-14 23:07:33 +0530

    chore: added test files to all folders

---
+++ variables and data types/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Library for input and output
+
+int main() {
+    // std::cout is used to print text to the console
+    std::cout << "Hello World!" << std::endl;
+    return 0; // Signals that the program finished successfully
+}
