311c9df362e972b8c36a7bed29f8c30ccea6aa00
Shiven Sharma <shiven676@gmail.com>
2026-05-21 00:36:27 +0530

    dsa: variables and datatypes

---
+++ variables and data types/patternprint.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // to print anything we use 
+    //'cout<<"hello World";'
+    
+    cout << "Shiven Sharma"<<" is here!\n\n";
+    //print the pattern:
+    // ****
+    // ***
+    // **
+    // *
+    cout <<"****\n***\n**\n*\n";
+
+    return 0;
+}
---
+++ variables and data types/variables.cpp
@@ -0,0 +1,16 @@
+// int , float are data types
+// let , const are variables
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 10;
+    int sum = a+b;
+    cout<<sum<<"\n";
+    int perimeter = 2*(a+b);
+    cout<<perimeter;
+
+    return 0;
+}
