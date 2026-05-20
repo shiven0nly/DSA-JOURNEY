9ef1f897a0ba6ecddfff99dace329a3e4382f860
Shiven Sharma <shiven676@gmail.com>
2026-05-20 14:37:10 +0530

    dsa: flowcharts and psuedocode

---
+++ flowcharts and psuedocode/problem1.cpp
@@ -0,0 +1,6 @@
+// sum of 2 no.s
+// start 
+// input a & b
+// sum = a+b
+// print sum
+// exit
---
+++ flowcharts and psuedocode/problem2.cpp
@@ -0,0 +1,7 @@
+// calculate simple interest
+
+// start
+// input P , R , T
+// Simple interest = P * R * T / 100
+// print simple interest
+// exit
