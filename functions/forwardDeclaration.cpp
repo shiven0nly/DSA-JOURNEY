c9a8bb956a6656639264da1511c48aee9cf58fb0
Shiven Sharma <shiven676@gmail.com>
2026-06-03 14:58:17 +0530

    dsa: started functions

---
+++ functions/forwardDeclaration.cpp
@@ -0,0 +1,17 @@
+// Declaration: the function's name, return type, and parameters (if any)
+// Definition: the body of the function
+
+#include <iostream>
+using namespace std;
+
+void sayHello()// decalration 
+{
+    cout<<"hello :)\n"; // definition
+}
+
+int main(){
+
+    
+
+    return 0;
+}
---
+++ functions/typesoffunctions.cpp
@@ -0,0 +1,12 @@
+// 1. void :- function that returns nothing , only we use to define the things or print something
+// function syntax:-
+//   returnType fName(){
+//        code here
+//        
+//      return someValue; // optional
+//}
+
+int main () {
+        //fName(); // calling functions
+    return 0;
+}
