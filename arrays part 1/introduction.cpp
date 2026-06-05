1587af392e3f334ae8187b376de441e9b4fa3339
Shiven Sharma <shiven676@gmail.com>
2026-06-05 19:18:13 +0530

    dsa: arrays started

---
+++ arrays part 1/introduction.cpp
@@ -0,0 +1,14 @@
+// arrays: are mostly use to store the data when large set is given..
+// linear collection of same type of elements that are stored together in contigous memory spaces.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {12,24321,53,12312};
+    char arr2[] = {'A', 'a', '23'};
+    // arrays ke index ka mtlb= position, it starts with 0th position, bhout saare log , ek line me khade hai toh , woh konse number per khade unko hum array bolte hai;
+    // size of arr = (no. of elements) * (size of variable type {int, char, float, double})
+    return 0;
+}
