5e996cb5d85b49e9f5fc5c76436c89dd56d02c72
Shiven Sharma <shiven676@gmail.com>
2026-06-07 01:03:25 +0530

    dsa: arrays part 2 started

---
+++ arrays part 2/printingSubArrays.cpp
@@ -0,0 +1,13 @@
+// subarray is continous part of an array
+// like arr[] = {1,2,3,4,5}
+// printing the terms individually = {1}, {2}, {3}, {4}, {5}
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1,2,3,4,5};
+    // prin the subarray
+
+    return 0;
+}
